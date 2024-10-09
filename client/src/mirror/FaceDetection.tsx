d300247ad35744626a56d7cdda2664c7a1dd82d5
chriistopherngo <ngo.chris@hotmail.com>
2024-10-09 21:27:23 +0200

    audio

--- client/src/mirror/FaceDetection.tsx
+++ client/src/mirror/FaceDetection.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./FaceDetection.css";
 import html2canvas from "html2canvas";
+import { FaCamera } from "react-icons/fa";
 
 declare const faceapi: any;
 
@@ -16,9 +17,12 @@ const FaceDetection: React.FC = () => {
   const [showPreview, setShowPreview] = useState(false); 
   const [showMoodPreview, setShowMoodPreview] = useState(false);
   const [moodImageData, setMoodImageData] = useState<string | null>(null); 
+  const [showCapturePhotoButtons, setshowCapturePhotoButtons] = useState(false);
+  const [shouldPlayAudio, setShouldPlayAudio] = useState(false);
+
+
 
   useEffect(() => {
-    
     const loadFaceApiScript = () => {
       return new Promise<void>((resolve, reject) => {
         const script = document.createElement("script");
@@ -70,9 +74,9 @@ const FaceDetection: React.FC = () => {
     };
   }, []);
 
-  useEffect(() => {
-    console.log("Face Expression Updated:", faceExpressions);
-  }, [faceExpressions]);
+  // useEffect(() => {
+  //   console.log("Face Expression Updated:", faceExpressions);
+  // }, [faceExpressions]);
 
   const handleVideoOnPlay = () => {
     if (videoRef.current && canvasRef.current && faceapi) {
@@ -120,48 +124,118 @@ const FaceDetection: React.FC = () => {
     }
   };
 
+  useEffect(() => {
+    happyAudio.current.loop = true;
+    sadAudio.current.loop = true;
+    angryAudio.current.loop = true;
+  }, []);
 
-  const takeMoodBasedScreenshot = () => {
-    if (videoRef.current && canvasRef.current) {
-        const video = videoRef.current;
-
-        // Create a canvas for the video frame
-        const canvas = document.createElement("canvas");
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-
-        const context = canvas.getContext("2d");
-        if (context) {
-            context.save();
-            context.scale(-1, 1); // Flip the video horizontally
-            context.drawImage(video, -canvas.width, 0, canvas.width, canvas.height);
-            context.restore();
-
-            // Use html2canvas to capture the specific HTML element
-            const elementToCapture = document.getElementById("specific");
-
-            if (elementToCapture) {
-              elementToCapture.style.opacity = "1"; 
-                // Capture with allowTaint set to true
-                html2canvas(elementToCapture, { allowTaint: true, backgroundColor: null  }).then((elementCanvas) => {
-                    const elementContext = elementCanvas.getContext("2d");
-                    if (elementContext) {
-                        // Draw the captured element on the original canvas
-                        context.drawImage(elementCanvas, 0, 0); // Adjust position as needed
-                    }
-
-                    // Convert to image and update state
-                    const img = canvas.toDataURL("image/png");
-                    setMoodImageData(img);
-                    setShowMoodPreview(true); // Show the mood-based image preview
-                });
-                elementToCapture.style.opacity = "0"; 
-            } else {
-                console.error("Element to capture not found!");
-            }
-        }
+  useEffect(() => {
+    if (shouldPlayAudio) {
+        playMoodAudio();
+        setShouldPlayAudio(false);
     }
+}, [shouldPlayAudio]);
+  
+const playMoodAudio = () => {
+  console.log("Playing mood audio", faceExpressions);
+  if (faceExpressions === "happy") {
+      console.log("Happy audio playing");
+      happyAudio.current.play().catch(error => {
+          console.error("Error playing sound:", error);
+      });
+  } else if (faceExpressions === "sad") {
+      console.log("Sad audio playing");
+      sadAudio.current.play().catch(error => {
+          console.error("Error playing sound:", error);
+      });
+  } else if (faceExpressions === "angry") {
+      console.log("Angry audio playing");
+      angryAudio.current.play().catch(error => {
+          console.error("Error playing sound:", error);
+      });
+  }
 };
+const stopMoodAudio = () => {
+  if (!happyAudio.current.paused) {
+    happyAudio.current.pause();
+    happyAudio.current.currentTime = 0; 
+}
+
+if (!sadAudio.current.paused) {
+    sadAudio.current.pause();
+    sadAudio.current.currentTime = 0;
+}
+
+if (!angryAudio.current.paused) {
+    angryAudio.current.pause();
+    angryAudio.current.currentTime = 0;
+}
+};
+
+const happyAudio =useRef(new Audio('/assets/happy.mp3')); 
+const sadAudio = useRef(new Audio('/assets/sad.mp3')); 
+const angryAudio = useRef(new Audio('/assets/angry.mp3')); 
+const takeMoodBasedScreenshot = () => {
+ 
+  if (videoRef.current && canvasRef.current) {
+      const video = videoRef.current;
+
+
+      const canvas = document.createElement("canvas");
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+
+      const context = canvas.getContext("2d");
+      if (context) {
+          context.save();
+          context.scale(-1, 1); // Flip the video horizontally
+          context.drawImage(video, -canvas.width, 0, canvas.width, canvas.height);
+          context.restore();
+
+
+          const elementsToCapture = document.getElementsByClassName("specific");
+
+          if (elementsToCapture.length > 0) {
+             
+              const positions = {
+                  topLeft: { x: 0, y: 0 }, // Sun position
+                  topRight: { x: canvas.width - 100, y: 0 } // Boodaflais position (adjusting for width of image)
+              };
+
+              // Iterate through each element and capture it
+              Array.from(elementsToCapture).forEach((element, index) => {
+                  const elementToCapture = element as HTMLElement; // Cast Element to HTMLElement
+                  
+                  elementToCapture.style.opacity = "1"; 
+
+                  // Capture with html2canvas
+                  html2canvas(elementToCapture, { allowTaint: true, backgroundColor: null }).then((elementCanvas) => {
+                      const elementContext = elementCanvas.getContext("2d");
+                      if (elementContext) {
+                          // Adjust position based on index
+                          const position = index === 0 ? positions.topLeft : positions.topRight;
+                          context.drawImage(elementCanvas, position.x, position.y); // Draw at calculated position
+                      }
+
+                      // Convert to image and update state
+                      const img = canvas.toDataURL("image/png");
+                      setMoodImageData(img);
+                      setShowMoodPreview(true); // Show the mood-based image preview
+                      setShouldPlayAudio(true);
+                
+                  });
+                  elementToCapture.style.opacity = "0"; 
+              });
+          } else {
+              console.error("Elements to capture not found!");
+          }
+      }
+  }
+};
+
+
+
 
   const takeScreenshot = () => {
     if (videoRef.current && canvasRef.current) {
@@ -264,6 +338,7 @@ const FaceDetection: React.FC = () => {
 
 
   const handleConfirmMoodScreenshot = () => {
+    stopMoodAudio();
     const link = document.createElement("a");
     link.href = moodImageData!;
     link.download = "mood_based_screenshot.png";
@@ -273,31 +348,86 @@ const FaceDetection: React.FC = () => {
   };
 
   const handleCancelMoodScreenshot = () => {
+    stopMoodAudio();
     setShowMoodPreview(false);
     setMoodImageData(null);
   };
 
+  const openCapturePhotoButtons = () => {
+    setshowCapturePhotoButtons((prev) => !prev);
+  }
+
   return (
     <div className={`relative h-screen ${blitz ? "blitz-effect" : ""}`}>
-      <div className="absolute z-10 mt-20">
-        <div className="w-[50px] h-[50px] bg-red-500" ></div>
 
-        {faceExpressions === "happy" &&
-          <img id="specific" className="w-[100px] h-[100px] opacity-0" src="/public/images/sun.png" alt="" />
+
+    {/* background filters, funke isje heilt skikkelig */}
+    
+    {/* {faceExpressions === "angry" && (
+      <div className="absolute w-full h-full overflow-hidden z-[-1]">
+          <img className="object-cover w-full h-full opacity specific" src="/images/rainfilter.png" alt="" />
+        </div>
+    )}
+
+    {faceExpressions === "happy" && (
+      <div className="absolute w-full h-full overflow-hidden z-[-1]">
+          <img className="object-cover w-full h-full opacity specific" src="/images/sunray.png" alt="" />
+        </div>
+    )} */}
+    
+
+
+      <div className="absolute z-10 w-full mt-20">
+    {faceExpressions === "happy" && (
+        <>
+            <img
+                className="specific w-[100px] h-[100px] opacity-0 absolute top-0 left-0" // Sun at top left
+                src="/images/sun.png"
+                alt="Sun"
+            />
+            <img
+                className="specific object-contain w-[50px] h-[auto] opacity-0 absolute top-0 right-0" // Boodaflais at top right
+                src="/images/boodaflais.png"
+                alt="Boodaflais"
+            />
+        </>
+    )}
+
+    {faceExpressions === "sad" && (<>
+      <img
+            className="specific w-[100px] h-[100px] opacity-0 absolute top-0 left-0"
+            src="/images/rain.png"
+            alt="Rain"
+        />
+      <img
+            className="specific object-contain w-[50px] h-[auto] opacity-0 absolute top-0 right-0" // Boodaflais at top right
+            src="/images/sadflower.png"
+            alt="sadflower"
+        />
+    </>
         
-        }
+    )}
 
-        {faceExpressions === "sad" &&
-          <img id="specific" className="w-[100px] h-[100px] opacity-0" src="/public/images/rain.png" alt="" />
+{faceExpressions === "angry" && (<>
+      <img
+            className="specific w-[100px] h-[100px] opacity-0 absolute top-0 left-0"
+            src="/images/angry.png"
+            alt="Rain"
+        />
+      <img
+            className="specific object-contain w-[50px] h-[auto] opacity-0 absolute top-0 right-0" // Boodaflais at top right
+            src="/images/lightningbolt.png"
+            alt="sadflower"
+        />
+    </>
         
-        }
+    )}
 
+    {faceExpressions === "neutral" && (
+        <img className="specific w-[100px] h-[100px] opacity-0" alt="" />
+    )}
+</div>
 
-        
-         
-
-
-      </div>
       <video
         ref={videoRef}
         className="object-cover w-full h-full inverted-video"
@@ -306,7 +436,7 @@ const FaceDetection: React.FC = () => {
         onPlay={handleVideoOnPlay}
       />
       <canvas ref={canvasRef} className="hidden" />
-      <div className="absolute top-0 left-0 p-4 text-white bg-black">
+      <div className="absolute top-0 left-0 p-4 text-white bg-black bg-opacity-50">
         <h1>Expression: {faceExpressions}</h1>
       </div>
       {countdown !== null && (
@@ -314,27 +444,39 @@ const FaceDetection: React.FC = () => {
           {countdown}
         </div>
       )}
-      <div className="absolute bottom-0 left-0 p-4">
-        <button
+      <div className="absolute bottom-0 left-0 flex p-4">
+
+      <button
           id="scrnsht_btn" 
-          className="p-2 text-white bg-blue-500"
+          className="rounded p-3 text-white bg-purple-500 hover:scale-[1.1] transform transition duration-150"
+          onClick={openCapturePhotoButtons}
+        >
+          <FaCamera />
+        </button>
+
+        {showCapturePhotoButtons && (<>
+          <button
+          id="scrnsht_btn" 
+          className="p-2 ml-2 text-white bg-blue-500 rounded"
           onClick={startCountdown}
         >
           Capture photo (Send to AI)
         </button>
         <button
-          className="p-2 ml-2 text-white bg-green-500"
+          className="p-2 ml-2 text-white bg-green-500 rounded"
           onClick={startCountdownMood}
         >
           Capture photo (Mood-based art)
         </button>
+        </>)}
+        
       </div>
 
       {showPreview && imageData && (
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="p-4 bg-white rounded">
             <img src={imageData} alt="Screenshot Preview" className="mb-4" />
-            <h2>Would you like to submit this image?</h2>
+            <h2>Would you like to submit this image to AI?</h2>
             <div className="flex justify-end gap-3">
               <button
                 className="p-2 text-white bg-blue-500"
@@ -353,26 +495,30 @@ const FaceDetection: React.FC = () => {
         </div>
       )}
 
-{showMoodPreview && moodImageData && (
-        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    {showMoodPreview && moodImageData && (
+      <>
+      <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="p-4 bg-white rounded">
-            <img src={moodImageData} alt="Mood Screenshot Preview" className="mb-4" />
-            <div>
-              <button
-                className="px-4 py-2 text-white bg-blue-500"
-                onClick={handleConfirmMoodScreenshot}
-              >
-                Confirm Mood Screenshot
-              </button>
-              <button
-                className="px-4 py-2 ml-2 text-gray-500"
-                onClick={handleCancelMoodScreenshot}
-              >
-                Cancel
-              </button>
-            </div>
+        <img src={moodImageData} alt="Mood Screenshot Preview" className="mb-4 " />
+        <div>
+          <button
+            className="px-4 py-2 text-white bg-blue-500"
+            onClick={handleConfirmMoodScreenshot}
+          >
+            Confirm Mood Screenshot
+          </button>
+          <button
+            className="px-4 py-2 ml-2 text-gray-500"
+            onClick={handleCancelMoodScreenshot}
+          >
+            Cancel
+          </button>
+        </div>
           </div>
         </div>
+      
+      </>
+      
       )}
     </div>
   );
