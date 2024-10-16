db8b173307467f217414efab1cc76a9cfcf98ee5
Andreas Christiansen <andycchhrr@gmail.com>
2024-10-16 14:43:16 +0200

    removed face api code :(

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import FaceDetection from "./mirror/FaceDetection";
+// import FaceDetection from "./mirror/FaceDetection";
 import MuseumDisplay from "./MuseumDisplay";
+import AiArtMirror from "./mirror/AiArtMirror";
 
 export default function App() {
   return (
     <Routes>
-      <Route index path='/' element={<FaceDetection />} />
+      <Route index path='/' element={<AiArtMirror />} />
       <Route path='/museum' element={<MuseumDisplay />} />
     </Routes>
   );
