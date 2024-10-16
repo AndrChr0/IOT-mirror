bc0a48c27e0a5c482c7ac348e9e93bb0dd26d194
Ninejar <olansk@stud.ntnu.no>
2024-10-16 14:44:34 +0200

    added route / controller for fetching art from database

---
+++ Backend/routes/carouselRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+import {getAllArt} from "../controllers/carouselController.js";
+
+
+router.get("/", getAllArt);
+
+
+export {router};
