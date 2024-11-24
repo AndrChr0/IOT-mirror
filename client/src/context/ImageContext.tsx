6dd0cb6b81c9ef0b587d461b00597d8bd80b1f75
Ninejar <olansk@stud.ntnu.no>
2024-11-24 22:35:17 +0100

    partially working might need web socks grrrr

--- client/src/context/ImageContext.tsx
+++ client/src/context/ImageContext.tsx
@@ -9,18 +9,18 @@ interface DBImage {
     generation_date: string;
     url: string;
     art_style: string;
-    art_title?: string;
-    __v: number;
+    art_title: string;
 }
 
 interface ImageContextType {
     imageState: string;
     setImageState: React.Dispatch<React.SetStateAction<string>>;
     DBImages: DBImage[];
-    setDBImages: React.Dispatch<React.SetStateAction<DBImage[]>>;
-    contextIndex: number;
+    // setDBImages: React.Dispatch<React.SetStateAction<DBImage[]>>;
+    currentIndex: number;
     setCurrentIndex: React.Dispatch<React.SetStateAction<number>>;
     fetchLatestArt: () => Promise<void>;
+    loading: boolean;
 }
 
 const ImageContext = createContext<ImageContextType | undefined>(undefined);
@@ -39,59 +39,72 @@ export const ImageProvider: React.FC<ImageProviderProps> = ({ children }) => {
     const [DBImages, setDBImages] = useState<DBImage[]>([]);
     const [currentIndex, setCurrentIndex] = useState<number>(0);
     const [isShuffled, setIsShuffled] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
       // pretend this queries DB :)
-      const fetchData = async () => {
-        const data: DBImage[] = [
-          {
-            _id: "673a20e35fe3d47bc0c9aa27",
-            generation_date: "2024-11-17T15:59:00.000Z",
-            url: "http://localhost:5353/images/1731862586189.png",
-            art_style: "Surrealism",
-            art_title: "A Noble Gamer's Retreat in Gjøvik.",
-            __v: 0
-          },
-          {
-            _id: "673a22135fe3d47bc0c9aa33",
-            generation_date: "2024-11-17T17:04:00.000Z",
-            url: "http://localhost:5353/images/1731863048923.png",
-            art_style: "Surrealism",
-            art_title: "\"A Glimpse of Creativity from Above in Gjøvik.\"",
-            __v: 0
-          },
-          {
-            _id: "673a22b35fe3d47bc0c9aa3c",
-            generation_date: "2024-11-16T23:00:00.000Z",
-            url: "http://localhost:5353/images/1731863212127.png",
-            art_style: "Surrealism",
-            art_title: "Engaging Digital Pursuits in Gjøvik",
-            __v: 0
-          },
-          {
-            _id: "673a2ed31b110b2d7312ea0f",
-            generation_date: "2024-11-16T23:00:00.000Z",
-            url: "http://localhost:5353/images/1731866261859.png",
-            art_style: "Surrealism",
-            art_title: "\"Whispers of Imagination: A Journey Through Dreamscapes\"",
-            __v: 0
-          },
-          {
-            _id: "673a377ebb7ca103f814fc0f",
-            generation_date: "2024-11-16T23:00:00.000Z",
-            url: "http://localhost:5353/images/1731868525347.png",
-            art_style: "Cubism",
-            art_title: "\"Joy in Motion\"",
-            __v: 0
-          }
-        ];
+      const fetchAiArt = async () => {
+        // const data: DBImage[] = [
+        //   {
+        //     _id: "673a20e35fe3d47bc0c9aa27",
+        //     generation_date: "2024-11-17T15:59:00.000Z",
+        //     url: "http://localhost:5353/images/1731862586189.png",
+        //     art_style: "Surrealism",
+        //     art_title: "A Noble Gamer's Retreat in Gjøvik.",
+        //     __v: 0
+        //   },
+        //   {
+        //     _id: "673a22135fe3d47bc0c9aa33",
+        //     generation_date: "2024-11-17T17:04:00.000Z",
+        //     url: "http://localhost:5353/images/1731863048923.png",
+        //     art_style: "Surrealism",
+        //     art_title: "\"A Glimpse of Creativity from Above in Gjøvik.\"",
+        //     __v: 0
+        //   },
+        //   {
+        //     _id: "673a22b35fe3d47bc0c9aa3c",
+        //     generation_date: "2024-11-16T23:00:00.000Z",
+        //     url: "http://localhost:5353/images/1731863212127.png",
+        //     art_style: "Surrealism",
+        //     art_title: "Engaging Digital Pursuits in Gjøvik",
+        //     __v: 0
+        //   },
+        //   {
+        //     _id: "673a2ed31b110b2d7312ea0f",
+        //     generation_date: "2024-11-16T23:00:00.000Z",
+        //     url: "http://localhost:5353/images/1731866261859.png",
+        //     art_style: "Surrealism",
+        //     art_title: "\"Whispers of Imagination: A Journey Through Dreamscapes\"",
+        //     __v: 0
+        //   },
+        //   {
+        //     _id: "673a377ebb7ca103f814fc0f",
+        //     generation_date: "2024-11-16T23:00:00.000Z",
+        //     url: "http://localhost:5353/images/1731868525347.png",
+        //     art_style: "Cubism",
+        //     art_title: "\"Joy in Motion\"",
+        //     __v: 0
+        //   }
+        // ];
         // populate states with dummy data for testing
-        setDBImages(data);
+        try {
+          const response = await fetch("http://localhost:5353/mirror", {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+          const data = await response.json();
+
+          setDBImages(data);
+
+          setLoading(false);
+        } catch (error) {
+          console.error("Error fetching AI art:", error);
+        }
       };
 
-      fetchData();
-      setImageState("khgnfyjrf");
-      setCurrentIndex(3);
+      fetchAiArt();
     }, []);
 
     useEffect(() => {
@@ -146,10 +159,11 @@ export const ImageProvider: React.FC<ImageProviderProps> = ({ children }) => {
         imageState,
         setImageState,
         DBImages,
-        setDBImages,
-        contextIndex: currentIndex,
+        // setDBImages,
+        currentIndex,
         setCurrentIndex,
         fetchLatestArt,
+        loading,
     };
 
 
